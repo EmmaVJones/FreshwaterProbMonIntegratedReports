e8c3d9aa5c3a352e6b3c75cf4c31e076059ffe8b
EmmaVJones <emma.v.jones@icloud.com>
2021-12-15 17:36:59 -0500

    version sent to J

--- 2022ProbChapter/global.R
+++ 2022ProbChapter/global.R
@@ -76,6 +76,15 @@ numbers = c('zero', 'one', 'two', 'three', 'four', 'five',
 readable = function (x) ifelse(x < 21, numbers[x + 1], x)
 
 
+# Capitalize first letter in a word easily
+capwords <- function(s, strict = FALSE) {
+  cap <- function(s) paste(toupper(substring(s, 1, 1)),
+                           {s <- substring(s, 2); if(strict) tolower(s) else s},
+                           sep = "", collapse = " " )
+  sapply(strsplit(s, split = " "), cap, USE.NAMES = !is.null(names(s)))
+}
+
+
 # Get data in correct format for micromap plots function
 
 #indicator <- 'TotHab'
